20369e1fa7b795482c8b7ae44b32fafd656d5818
Maddy Andrade <maddy.andrade@ithaka.org>
2022-02-20 15:45:58 -0500

    Start basic test assumptions

---
+++ src/__tests__/student.test.js
@@ -0,0 +1,17 @@
+test('student enters code with if statement', () => {
+    // render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+});
+
+test('student enters code without if statement', () => {
+    // render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+});
+
+test('student enters code with nested methods', () => {
+    // render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+});
