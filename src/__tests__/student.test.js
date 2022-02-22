ac2030e4f666b6155db7504f9608c9ffbd59cbce
Maddy Andrade <maddy.andrade@ithaka.org>
2022-02-22 09:16:10 -0500

    Test for nested function type

--- src/__tests__/student.test.js
+++ src/__tests__/student.test.js
@@ -1,26 +1,37 @@
 import exp from "constants";
-import { setVariable } from "../helpers/student-code-snippets"
-import { nodeVisit } from "../helpers/methods"
+import { setVariable, stringIfStatement } from "../helpers/student-code-snippets"
+import { traverseAstNodes } from "../helpers/methods"
 
 let acorn = require("acorn");
 
 test('Code HAS specified functionality', () => {
 
     const testParseSetVariable = acorn.parse(setVariable, { ecmaVersion: 2020 });
-    const resultOfTraversing = nodeVisit(testParseSetVariable, "VariableDeclaration");
-    expect(resultOfTraversing).toBe("Great, you used a VariableDeclaration in your code!")
+    const resultOfTraversing = traverseAstNodes(testParseSetVariable, "VariableDeclaration");
+    expect(resultOfTraversing).toBe(true)
+    
+
+    const resultOfTraversing1 = traverseAstNodes(testParseSetVariable, "BlockStatement");
+    expect(resultOfTraversing1).toBe(false)
+
+    const testStringIfStatement = acorn.parse(stringIfStatement, { ecmaVersion: 2020 });
+
+    const resultStringIfStatement = traverseAstNodes(testStringIfStatement, "IfStatement");
+    expect(resultStringIfStatement).toBe(true)
+
 });
 
-test('Code MUST HAVE specified functionality', () => {
 
-    const testParseSetVariable = acorn.parse(setVariable, { ecmaVersion: 2020 });
-    const resultOfTraversing = nodeVisit(testParseSetVariable, "VariableDeclaration");
-    expect(resultOfTraversing).toBe("This program MUST use a 'for loop' and a 'variable declaration'.")
-});
+// test('Code MUST HAVE specified functionality', () => {
 
-test('Code DOES NOT HAVE specified functionality', () => {
+//     const testParseSetVariable = acorn.parse(setVariable, { ecmaVersion: 2020 });
+//     const resultOfTraversing = traverseAstNodes(testParseSetVariable, "VariableDeclaration");
+//     expect(resultOfTraversing).toBe("This program MUST use a 'for loop' and a 'variable declaration'.")
+// });
 
-    const testParseSetVariable = acorn.parse(setVariable, { ecmaVersion: 2020 });
-    const resultOfTraversing = nodeVisit(testParseSetVariable, "VariableDeclaration");
-    expect(resultOfTraversing).toBe("This program MUST NOT use a VariableDeclaration.")
-});
+// test('Code DOES NOT HAVE specified functionality', () => {
+
+//     const testParseSetVariable = acorn.parse(setVariable, { ecmaVersion: 2020 });
+//     const resultOfTraversing = traverseAstNodes(testParseSetVariable, "VariableDeclaration");
+//     expect(resultOfTraversing).toBe("This program MUST NOT use a VariableDeclaration.")
+// });
